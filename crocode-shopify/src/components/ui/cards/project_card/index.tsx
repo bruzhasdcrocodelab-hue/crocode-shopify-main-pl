0acdf0f3d26e76d2eb655bfe1f4c47e2f4e145ae
Daria Bruzhas <bruzhas.d.crocodelab@gmail.com>
2026-02-23 14:22:43 +0300

    Fixing a URL error

--- crocode-shopify/src/components/ui/cards/project_card/index.tsx
+++ crocode-shopify/src/components/ui/cards/project_card/index.tsx
@@ -13,7 +13,7 @@ const ProjectCard = ({className, project}: TProps) => {
   return (
     <div className={`${styles.card} ${className}`}>
       <Background desktop={project?.cardImage?.image.asset.url || '/images/cards/card-project.png'} alt={project?.cardImage?.altText || `Project ${project?.title}`}/>
-      <Link href={`our-work/${project?.slug.current}`} className={styles.card__link}>
+      <Link href={`/our-work/${project?.slug.current}`} className={styles.card__link}>
         <span className={styles.card__title}>{project?.title}</span>
         <span className={styles.card__label}>{project?.workDone}</span>
       </Link>
